9443b4e6932be37b47c33c56e9f547749cf2d1e2
Maheerah Anwar <maheerahanwar@gmail.com>
2025-01-08 20:59:56 -0500

    add property sql function

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -111,6 +111,7 @@ const getAllProperties = function (options, limit = 10) {
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
+  
   `;
 
   // 3. Add filters based on options
@@ -157,11 +158,61 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // 1
+  const queryString = `
+    INSERT INTO properties (
+      owner_id, 
+      title, 
+      description, 
+      thumbnail_photo_url, 
+      cover_photo_url, 
+      cost_per_night, 
+      street, 
+      city, 
+      province, 
+      post_code, 
+      country, 
+      parking_spaces, 
+      number_of_bathrooms, 
+      number_of_bedrooms
+    )
+    VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+    )
+    RETURNING *;
+  `;
+
+  // 2
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+  ];
+
+  // 3
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      console.log('New property added:', result.rows[0]); // Debugging output
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error('Error adding property:', err.message);
+      throw err;
+    });
 };
+
 
 module.exports = {
   getUserWithEmail,
